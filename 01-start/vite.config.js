df6f88dc276d579ba11e2aca26e31b9c70b15a5c
疏狂难除 <12355288+its-hard-to-be-distracted@user.noreply.gitee.com>
2024-04-29 02:06:03 +0800

    build

--- 01-start/vite.config.js
+++ 01-start/vite.config.js
@@ -3,10 +3,12 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [react()],
   build: {
     outDir: "../build",
     assetsDir: "assets",
+
     rollupOptions: {
       input: "./index.html",
       output: {
