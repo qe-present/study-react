7a7534c012b2d32d43c570f9a1311ae03108ec9b
qe617 <2664481691@qq.com>
2024-05-09 01:22:30 +0800

    add

--- 04-styling/src/components/AuthInputs.jsx
+++ 04-styling/src/components/AuthInputs.jsx
@@ -1,14 +1,9 @@
 import {useState} from 'react';
-import styled from 'styled-components';
+
 import Button from './Button';
 import Input from './Input';
 
-let ControlDiv = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 0.5rem;
-    margin-bottom: 1.5rem;
-`
+
 
 export default function AuthInputs() {
     const [enteredEmail, setEnteredEmail] = useState('');
@@ -31,8 +26,9 @@ export default function AuthInputs() {
     const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
     return (
-        <div id="auth-inputs">
-            <ControlDiv>
+        <div id="auth-inputs"
+             className="w-full max-w-sm p-8 mx-auto rounded shadow-md bg-gradient-to-b from-stone-700 to-stone-800">
+            <div className="flex flex-col gap-2 mb-6">
                 <Input
                     label="Email"
                     type="email"
@@ -47,9 +43,9 @@ export default function AuthInputs() {
                         handleInputChange('password', event.target.value)
                     }
                 />
-            </ControlDiv>
-            <div className="actions">
-                <button type="button" className="text-button">
+            </div>
+            <div className="flex justify-end gap-4 ">
+                <button type="button" className="text-amber-400 hover:text-amber-500">
                     Create a new account
                 </button>
                 <Button onClick={handleLogin}>Sign In</Button>
