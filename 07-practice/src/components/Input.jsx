6c3970466237971efc5d18ee246732230ce9eaa8
qe617 <2664481691@qq.com>
2024-05-15 02:00:43 +0800

    add

--- 07-practice/src/components/Input.jsx
+++ 07-practice/src/components/Input.jsx
@@ -1,8 +1,13 @@
 export default function Input({isTextArea,label,...props}) {
+    const classes = 'w-full p-1 border-b-2 rounded-sm boarder-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600 focus:bg-stone-100'
     return (
-        <p>
-            <label>{label}</label>
-            {isTextArea ? <textarea {...props}></textarea> : <input {...props}/>}
+        <p className='flex flex-col gap-1 my-4'>
+            <label className='text-sm font-bold uppercase text-stone-500 '>{label}</label>
+            {isTextArea ?
+                <textarea className={classes}
+                          {...props}></textarea>
+                :
+                <input className={classes} {...props}/>}
         </p>
     )
 }
