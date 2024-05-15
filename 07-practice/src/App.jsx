b162e62909deecd784ad982a53c76ba842da2f3b
qe617 <2664481691@qq.com>
2024-05-15 11:17:00 +0800

    07

--- 07-practice/src/App.jsx
+++ 07-practice/src/App.jsx
@@ -1,12 +1,33 @@
 import ProjectsSidebar from "./components/ProjectsSidebar.jsx";
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
 import NewProject from "./components/NewProject.jsx";
+import React, {useState} from "react";
+
 function App() {
-  return (
-    <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar />
-        <NewProject></NewProject>
-    </main>
-  );
+    const [projectsState, setProjectState] = useState({
+        selectedProjectId: undefined,
+        projects: []
+    })
+    function handleStartAddProject(projectId) {
+        setProjectState(prevState => {
+            return {
+                ...prevState,
+                selectedProjectId: null
+            }
+        })
+    }
+    let content;
+    if (projectsState.selectedProjectId === null) {
+        content = <NewProject />
+    } else {
+        content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>
+    }
+    return (
+        <main className="h-screen my-8 flex gap-8">
+            <ProjectsSidebar  onStartAddProject={handleStartAddProject}/>
+            {content}
+        </main>
+    );
 }
 
 export default App;
