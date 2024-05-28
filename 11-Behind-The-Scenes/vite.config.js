bac2e12b0ea84bd9e3318b6e031bb4f74ae424a9
qe-present <2664481691@qq.com>
2024-05-29 01:41:32 +0800

    add

--- 11-Behind-The-Scenes/vite.config.js
+++ 11-Behind-The-Scenes/vite.config.js
@@ -1,20 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import MillionLint from '@million/lint';
+import million from 'million/compiler';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
-  base:'./',
-  build:{
-    outDir:'../10-build',
-    assetsDir:'assets',
-    rollupOptions:{
-      input:'index.html',
-      output:{
-        entryFileNames:'[name].[hash].js',
-        chunkFileNames:'[name].[hash].js',
-        assetFileNames:'assets/[name].[hash][extname]'
+  plugins: [million.vite({auto: true}), react()],
+  base: './',
+  build: {
+    outDir: '../10-build',
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: 'index.html',
+      output: {
+        entryFileNames: '[name].[hash].js',
+        chunkFileNames: '[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash][extname]'
       }
     }
   }
-})
+});
