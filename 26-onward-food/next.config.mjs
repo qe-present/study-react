7c15e2ce957b84691d109568ff86878f3b1d047d
qe-present <2664481691@qq.com>
2025-03-28 16:36:22 +0800

    add

--- 26-onward-food/next.config.mjs
+++ 26-onward-food/next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env:{
+        TURSO_DATABASE_URL:process.env.TURSO_DATABASE_URL,
+        TURSO_AUTH_TOKEN:process.env.TURSO_AUTH_TOKEN
+    }
+};
 
 export default nextConfig;
