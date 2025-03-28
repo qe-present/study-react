0be64dec644060d6c1d3fc44f072b4b19554e570
qe-present <2664481691@qq.com>
2025-03-28 16:43:56 +0800

    add

--- 26-onward-food/utils/connext.js
+++ 26-onward-food/utils/connext.js
@@ -2,8 +2,8 @@ import {createClient} from '@libsql/client'
 console.log(process.env.TURSO_DATABASE_URL)
 // 初始化数据库客户端
 const turso = createClient({
-    url:process.env.TURSO_DATABASE_URL,
-    authToken:process.env.TURSO_AUTH_TOKEN
+    url:process.env.TURSO_DATABASE_URL||'libsql://store-qe-present.turso.io',
+    authToken:process.env.TURSO_AUTH_TOKEN||"eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3NDI5ODExNjQsImlkIjoiOWI1Y2QwM2MtYTQ2YS00Y2E5LTkyODQtYzFmMTQ3OWRkOGQwIiwicmlkIjoiNGMyNDc1ZDktNzEwNi00Yjc3LTljOGItMDQxZjgyY2UwNWVlIn0.i_3qm74FOxWMVFmLRLUaE28Rzlot9jbgqAIxeB6vOrC09N7MOubnnLeGbkgJxqFbZwEEuhVwMYXr25CziFcyAw"
 });
 
 export default turso
